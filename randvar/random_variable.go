231fa4eff7cff009f30c4a63e896b59e35a2caaa
Dan Stoian <danmihaistoian@gmail.com>
2021-02-15 20:26:12 +0200

    added tests; switched to float64

--- randvar/random_variable.go
+++ randvar/random_variable.go
@@ -6,14 +6,14 @@ import "fmt"
 type RandVar struct {
 	Event        string
 	isEqualDist  bool
-	probabilites map[string]float32
+	probabilites map[string]float64
 }
 
 // New creates a new random variable
 func New(event string, domain []string) *RandVar {
 	ln := len(domain)
-	domProb := make(map[string]float32, ln)
-	prob := float32(1 / ln)
+	domProb := make(map[string]float64, ln)
+	prob := 1 / float64(ln)
 
 	for _, d := range domain {
 		domProb[d] = prob
@@ -27,13 +27,13 @@ func New(event string, domain []string) *RandVar {
 }
 
 // NewWithDist creates a random variable with unequal distribution
-func NewWithDist(event string, domDist map[string]float32) (*RandVar, error) {
+func NewWithDist(event string, domDist map[string]float64) (*RandVar, error) {
 	if err := VerifyDist(event, domDist); err != nil {
 		return nil, err
 	}
 
 	ln := len(domDist)
-	domProb := make(map[string]float32, ln)
+	domProb := make(map[string]float64, ln)
 	for e, d := range domDist {
 		domProb[e] = d
 	}
@@ -46,7 +46,7 @@ func NewWithDist(event string, domDist map[string]float32) (*RandVar, error) {
 }
 
 // Probability calculates the probanility of a given event
-func (r RandVar) Probability(event string) (float32, error) {
+func (r RandVar) Probability(event string) (float64, error) {
 	if prob, ok := r.probabilites[event]; ok {
 		return prob, nil
 	}
